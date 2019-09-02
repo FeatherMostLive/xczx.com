f6bfb6217a8b64418f5b9f887244c514ae8c1cbf
FeatherMostLive <1984775124@qq.com>
2019-09-02 20:11:06 +0800

    我倒要看看我记得不记得

--- hello_demo/src/com/Feather1/Hello.java
+++ hello_demo/src/com/Feather1/Hello.java
@@ -2,6 +2,6 @@ package com.Feather1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("这是一首简单的小情歌");
+        System.out.println("按F进入坦克");
     }
 }
